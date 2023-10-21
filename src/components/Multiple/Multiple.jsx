d27c4700e81d1b5ab4907e99be6dc1a048a3233b
Lucianna Mendonca <luciannamend@gmail.com>
2023-10-20 21:16:17 -0400

    added validation on price and changed buttons

--- src/components/Multiple/Multiple.jsx
+++ src/components/Multiple/Multiple.jsx
@@ -3,136 +3,134 @@ import "./multiple.css";
 
 //NEW PRODUCT REGISTRATION FORM
 export default function Multiple() {
-    const [formData, setFormData] = useState({
-        name: "",
-        description: "",    
-        category: "",
-        quantity: 0,
-        price: 0,
-    });
-    
-    // handle input change
-    const handleChange = (event) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    description: "",
+    category: "",
+    quantity: 0,
+    price: "",
+  });
+
+  // handle input change
+  const handleChange = (event) => {
     const { name, value } = event.target;
-    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
-    };
+    const decimalRegex = /^[0-9]+(\.[0-9]+)?/;    
+    if (name !== "price" || decimalRegex.test(value)) {
+      setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+    }
+  };
 
-    // handle category selection 
-    const [selectedOption, setSelectedOption] = useState('eletronics');  
+  // handle category selection
+  const [selectedOption, setSelectedOption] = useState("eletronics");
 
-    const handleDropdownChange = (event) => {
-        setSelectedOption(event.target.value);
-    };
+  const handleDropdownChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
 
-    // click on submit button shows an alert with all information submitted
-    function handleSubmit() {
-        alert(
-            `Name: ${formData.name}, 
+  // click on submit button shows an alert with all information submitted
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(
+      `Name: ${formData.name}, 
             Description: ${formData.description},       
             Category: ${selectedOption},
             Quantity: ${formData.quantity}, 
             Price: ${formData.price},`
-        );
-    };
+    );
+  };
 
-    // click on cancel button resets the page 
-    function handleCancel() {
-        setFormData({
-            name: '',
-            description: '',
-            quantity: 0,
-            price: 0,
-        });
-        setSelectedOption('');
-    }
+  // click on cancel button resets the page
+  function handleCancel() {
+    setFormData({
+      name: "",
+      description: "",
+      quantity: 0,
+      price: "",
+    });
+    setSelectedOption("");
+  }
 
-    //Form display
-    return (
+  //Form display
+  return (
+    <div> 
         <form onSubmit={handleSubmit} className="multiple">
+        {/*Name*/}
+        <label className="multiple__text" htmlFor="name">
+            Name:
+        </label>
+        <input
+            type="text"
+            id="name"
+            className="multiple__input"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+        />
 
-            {/*Name*/}
-            <label className="multiple__text" htmlFor="name">
-                Name:
-            </label>
-            <input
-                type="text"
-                id="name"
-                className="multiple__input"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-            />
+        {/*Description*/}
+        <label className="multiple__text" htmlFor="description">
+            Description:
+        </label>
+        <input
+            type="description"
+            id="description"
+            className="multiple__input"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+        />
 
-            {/*Description*/}
-            <label className="multiple__text" htmlFor="description">
-                Description: 
-            </label>
-            <input
-                type="description"
-                id="description"
-                className="multiple__input"
-                name="description"
-                value={formData.description}
-                onChange={handleChange}
-            />
+        {/*Category*/}
+        <label className="dropdown" htmlFor="category">
+            Category:
+        </label>
+        <select value={selectedOption} onChange={handleDropdownChange}>
+            <option id="electronics" name="electronics" value="electronics">
+            {" "}
+            Electronics
+            </option>
+            <option id="clothing" name="clothing" value="clothing">
+            {" "}
+            Clothing
+            </option>
+            <option id="groceries" name="groceries" value="groceries">
+            {" "}
+            Groceries
+            </option>
+            <option id="home" name="home" value="home">
+            {" "}
+            Home
+            </option>
+        </select>
 
-            {/*Category*/}
-            <label className="dropdown" htmlFor="category">
-                Category:
-            </label> 
-            <select value={selectedOption} onChange={handleDropdownChange}>
-                <option 
-                    id="electronics"  
-                    name="electronics"      
-                    value="electronics"
-                    > Electronics
-                </option>
-                <option  
-                    id="clothing"     
-                    name="clothing"          
-                    value="clothing"
-                    > Clothing
-                </option>
-                <option  
-                    id="groceries"     
-                    name="groceries"          
-                    value="groceries"
-                    > Groceries
-                </option>
-                <option 
-                    id="home" 
-                    name="home"               
-                    value="home"
-                    > Home 
-                </option>
-            </select>        
+        {/*Quantity*/}
+        <label className="multiple__text" htmlFor="quantity">
+            Quantity:
+        </label>
+        <input
+            type="number"
+            id="quantity"
+            className="multiple__input"
+            name="quantity"
+            value={formData.quantity}
+            onChange={handleChange}
+        />
 
-            {/*Quantity*/}
-            <label className="multiple__text" htmlFor="quantity">
-                Quantity:
-            </label>
-            <input
-                type="number"
-                id="quantity"
-                className="multiple__input"
-                name="quantity"
-                value={formData.quantity}
-                onChange={handleChange}
-            />
+        {/*Price*/}
+        <label className="multiple__text" htmlFor="price">
+            Price:
+        </label>
+        <input
+            type="text"
+            id="price"
+            className="multiple__input"
+            name="price"
+            value={formData.price}
+            onChange={handleChange}
+        />
+        </form>
 
-            {/*Price*/}
-            <label className="multiple__text" htmlFor="price">
-                Price:
-            </label>
-            <input
-                type="text"
-                id="price"
-                className="multiple__input"
-                name="price"
-                value={formData.price}
-                onChange={handleChange}
-            />
-
+        <div>
             {/*Submit button*/}
             <button className="multiple__button" type="submit" onClick={handleSubmit}>
                 Submit
@@ -142,6 +140,7 @@ export default function Multiple() {
             <button className="multiple__button" type="cancel" onClick={handleCancel}>
                 Cancel
             </button>
-        </form>
-    )
+        </div>
+    </div>
+  );
 }
